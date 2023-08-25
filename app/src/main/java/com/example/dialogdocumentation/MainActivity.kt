f842100cd87e1481d0a8114dd4e8d2e1440b4218
kaudi <19kaudi98@gmail.com>
2023-08-25 15:05:00 -0400

    This is 5th commit

--- app/src/main/java/com/example/dialogdocumentation/MainActivity.kt
+++ app/src/main/java/com/example/dialogdocumentation/MainActivity.kt
@@ -15,6 +15,7 @@ class MainActivity : AppCompatActivity() {
         setContentView(R.layout.activity_main)
 
         print("This is new stuff in 2nd commit.")
+        "5th commit reverting test"
 
         textElement = findViewById<TextView>(R.id.textElement)
 
